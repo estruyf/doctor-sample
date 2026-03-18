16be83c62d221c4939db7425342c895ccc7ffbd1
Elio Struyf <elio@struyfconsulting.be>
2026-03-18 10:59:04 +0100

    Restore sample shortcode implementation in CommonJS format

--- shortcodes/sample.cjs
+++ shortcodes/sample.cjs
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
   name: "sample",
   render: (attributes, html) => {
     return `<div class="custom-shortcode">External Shortcode - Name: ${attributes.name} - HTML: ${html}</div>`
-  },
+  }
 };
