b8e6e9b95418af1d5c7283db5fafcdc897f9c420
Elio Struyf <elio@struyfconsulting.be>
2026-03-18 10:52:46 +0100

    Add sample shortcode module

--- shortcodes/sample.cjs
+++ shortcodes/sample.cjs
@@ -1,7 +1,6 @@
-
-module.exports = {
+export default {
   name: "sample",
   render: (attributes, html) => {
     return `<div class="custom-shortcode">External Shortcode - Name: ${attributes.name} - HTML: ${html}</div>`
-  }
+  },
 };
